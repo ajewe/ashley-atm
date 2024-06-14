584bb09dbb97d6f7009a61a01d0167d6ca4d8270
ajewe <ajewe@utexas.edu>
2024-06-14 10:08:43 -0500

    feat: setup base container

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-function App() {
+import { useState } from 'react';
+
+const App: React.FC = () => {
+  const [accountPin, setAccountPin] = useState<string>('');
+
   return (
-    <div>
-      <div className='container bg-green-300 mx-auto'>Start!</div>
+    <div className='h-screen'>
+      <div className='w-full flex justify-between p-4 bg-gray-200'>
+        <div>ATM</div>
+        <div>Inactivate</div>
+      </div>
+      <div className='container flex flex-col justify-center items-center bg-green-300 mx-auto h-full'>
+        Start!
+        {accountPin ? <div>Has account pin!</div> : <div>No account pin!</div>}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
